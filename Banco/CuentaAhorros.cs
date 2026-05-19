d850f312b177b69e6cb29720b268775b93a12283
Navegador <Navegador@RCA-L1306-09-11>
2026-05-19 11:04:17 -0300

    Agregar archivos de proyecto.

---
+++ Banco/CuentaAhorros.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+     public class CuentaAhorros : CuentaBancaria
+    {
+        //contador de retiros (max 3)
+        private int retiros = 0;
+
+        public CuentaAhorros(double saldoInicial) : base(saldoInicial)
+        {
+
+        }
+
+    }
+}
---
+++ Banco/CuentaBancaria.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    public abstract class CuentaBancaria
+    {
+        public int NumeroCuenta { get; set; }
+        public string Titular { get; protected set; }
+        //saldo de la cuenta
+        protected double saldo;
+
+        //constructor para inicializar el saldo
+        public CuentaBancaria(double saldoInicial)
+        {
+            saldo = saldoInicial;
+        }
+
+        //metodo abstracto para retirar (cada cuenta lo imprementa distinto)
+        public abstract void Retirar(double monto);
+
+        //metodo para depositar dinero
+        public virtual void Depositar(double monto)
+        {
+            try
+            {
+                //validacion de datos
+                if (monto <= 0)
+                {
+                    throw new ArgumentException("el deposito debe ser mayor a 0");
+                }
+
+                saldo += monto;
+                Console.WriteLine("deposito realizado. Saldo " + saldo);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("error: " + e.Message);
+            }
+        }
+
+        //consultar saldo
+        public double ConsultarSaldo()
+        {
+            return saldo;
+        }
+    } 
+}
+
+        
+
+
+
+    
+
